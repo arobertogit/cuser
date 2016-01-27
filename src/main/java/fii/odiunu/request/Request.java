7468ee734f00a2eae058edea6b47e140d111c7df
Ervin Necula <ervinnecula@yahoo.com>
2016-01-27 10:21:55 +0200

    Changed .nt in/out to XML/RDF

--- src/main/java/fii/odiunu/request/Request.java
+++ src/main/java/fii/odiunu/request/Request.java
@@ -1,7 +1,7 @@
 package fii.odiunu.request;
 
 import java.io.BufferedReader;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.net.URL;
 
@@ -13,6 +13,7 @@ import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
+import org.apache.jena.datatypes.xsd.XSDDatatype;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -105,111 +106,6 @@ public class Request {
         return linkToSong;
     }*/
 
-    public void readMenuFromRDF(String keyword) {
-        Model model = ModelFactory.createDefaultModel();
-
-        //de verificat calea
-        model.read("/web/resources/" + keyword + ".nt", "NTRIPLES");
-
-        for (int i = 0; i < 10; i++) {
-            Resource video = model.getResource("product" + i);
-            Property p1 = model.createProperty("name");
-            Property p2 = model.createProperty("ingredients");
-            Property p3 = model.createProperty("price");
-            Property p4 = model.createProperty("tags");
-
-            // TODO
-            System.out.println(video.getProperty(p1).getString());
-            System.out.println(video.getProperty(p2).getString());
-            System.out.println(video.getProperty(p3).getString());
-            System.out.println(video.getProperty(p4).getString());
-            /* pass variables to client*/
-        }
-    }
-
-    public void writeVideosToRDF(String keyword) {
-
-        String api_key = "AIzaSyCZO2nHBNMSGgRg4VHMZ9P8dWT0H23J-Fc";
-        String yt_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="
-                + keyword + "&type=video&videoCaption=closedCaption&key=" + api_key + "&format=5&maxResults=10&v=2";
-        String line, stringArray;
-        StringBuilder stringArrayBuilder = new StringBuilder();
-
-        String titleOfVideo;
-        String description;
-        String thumbnailURL;
-        String videoId;
-
-        Model model = ModelFactory.createDefaultModel();
-
-        try {
-            URL url = new URL(yt_url);
-            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-            while ((line = br.readLine()) != null) {
-                stringArrayBuilder = stringArrayBuilder.append(line);
-            }
-            stringArray = stringArrayBuilder.toString();
-
-            JSONObject nodeRoot = new JSONObject(stringArray);
-            JSONArray jsonArray = (JSONArray) nodeRoot.get("items");
-
-
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject obj = jsonArray.getJSONObject(i);
-
-                JSONObject snippet = (JSONObject) obj.get("snippet");
-
-                description = (String) snippet.get("description");
-                titleOfVideo = (String) snippet.get("title");
-
-
-                JSONObject thumbnails = (JSONObject) snippet.get("thumbnails");
-                JSONObject thumbnail = (JSONObject) thumbnails.get("high");
-                thumbnailURL = (String) thumbnail.get("url");
-
-                JSONObject id = (JSONObject) obj.get("id");
-                videoId = (String) id.get("videoId");
-
-
-                Resource video = model.createResource("video" + i);
-                Property p1 = model.createProperty("title");
-                video.addProperty(p1, titleOfVideo);
-                Property p2 = model.createProperty("description");
-                video.addProperty(p2, description);
-                Property p3 = model.createProperty("thumbnail");
-                video.addProperty(p3, thumbnailURL);
-                Property p4 = model.createProperty("id");
-                video.addProperty(p4, videoId);
-
-            }
-            FileOutputStream fos = new FileOutputStream(keyword + ".nt");
-
-            RDFDataMgr.write(fos, model, Lang.NTRIPLES);
-
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-    }
-
-    public void readVideosFromRDF(String keyword) {
-        Model model = ModelFactory.createDefaultModel();
-        model.read(keyword + ".nt", "NTRIPLES");
-
-        for (int i = 0; i < 10; i++) {
-            Resource video = model.getResource("video" + i);
-            Property p1 = model.createProperty("title");
-            Property p2 = model.createProperty("description");
-            Property p3 = model.createProperty("thumbnail");
-            Property p4 = model.createProperty("id");
-
-            //use it to send resources to user. p4 (id) contains the id of the video
-
-        }
-
-    }
-
     public String writeMusicToRDF(String keyword) {
 
         JSONArray jsonArray = new JSONArray();
@@ -223,6 +119,15 @@ public class Request {
         String musicURL;
 
         Model model = ModelFactory.createDefaultModel();
+
+        String nsPrefix = "https://schema.org/AudioObject#";
+
+        Property p1 = model.createProperty(nsPrefix, "title");
+        Property p2 = model.createProperty(nsPrefix, "description");
+        Property p3 = model.createProperty(nsPrefix, "url");
+        Property p4 = model.createProperty(nsPrefix, "id");
+        Property p5 = model.createProperty(nsPrefix, "genre");
+        model.setNsPrefix( "music", nsPrefix );
 
         try {
             URL url = new URL("http://api.soundcloud.com/tracks?client_id=ffcaccc2a3bf0998c26d5a980a8b8607&q=" + keyword + "&limit=50");
@@ -254,24 +159,17 @@ public class Request {
                         musicGenre = "";
                     }
 
-
-                    Resource music = model.createResource("music"+i);
-                    Property p1 = model.createProperty("title");
-                    music.addProperty(p1, titleOfMusic);
-                    Property p2 = model.createProperty("description");
-                    music.addProperty(p2, description);
-                    Property p3 = model.createProperty("url");
-                    music.addProperty(p3, musicURL);
-                    Property p4 = model.createProperty("id");
-                    music.addProperty(p4, musicId);
-                    Property p5 = model.createProperty("genre");
-                    music.addProperty(p5, musicGenre);
+                    Resource music = model.createResource(nsPrefix + "music"+i);
+                    music.addProperty(p1, titleOfMusic, XSDDatatype.XSDstring);
+                    music.addProperty(p2, description, XSDDatatype.XSDstring);
+                    music.addProperty(p3, musicURL, XSDDatatype.XSDanyURI);
+                    music.addProperty(p4, musicId, XSDDatatype.XSDpositiveInteger);
+                    music.addProperty(p5, musicGenre, XSDDatatype.XSDstring);
                 }
             }
 
-            FileOutputStream fos = new FileOutputStream("ms-"+keyword+".nt");
-
-            RDFDataMgr.write(fos, model, Lang.NTRIPLES);
+            FileWriter out = new FileWriter("ms-"+keyword+".xml" );
+            model.write( out, "RDF/XML" );
 
 
         } catch (Exception ex) {
@@ -282,18 +180,88 @@ public class Request {
 
     public void readMusicFromRDF(String keyword){
         Model model = ModelFactory.createDefaultModel();
-        model.read("ms-"+keyword+".nt", "NTRIPLES");
+        model.read("ms-"+keyword+".xml", "RDF/XML");
 
-        for (int i = 0; i < 10; i++) {
-            Resource music = model.getResource("music" + i);
-            Property p1 = model.createProperty("title");
-            Property p2 = model.createProperty("description");
-            Property p3 = model.createProperty("url");
-            Property p4 = model.createProperty("id");
-            Property p5 = model.createProperty("genre");
+        RDFDataMgr.write(System.out, model, Lang.RDFXML);
+
+    }
+
+    public void writeVideosToRDF(String keyword){
+
+        String api_key = "AIzaSyCZO2nHBNMSGgRg4VHMZ9P8dWT0H23J-Fc";
+        String yt_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="
+                + keyword + "&type=video&videoCaption=closedCaption&key=" + api_key + "&format=5&maxResults=10&v=2";
+        String line = "", stringArray;
+        StringBuilder stringArrayBuilder = new StringBuilder();
+
+        String titleOfVideo;
+        String description;
+        String thumbnailURL;
+        String videoId;
+
+        Model model = ModelFactory.createDefaultModel();
+
+        String nsPrefix = "https://schema.org/VideoObject#";
+
+        Property p1 = model.createProperty(nsPrefix, "title");
+        Property p2 = model.createProperty(nsPrefix, "description");
+        Property p3 = model.createProperty(nsPrefix, "thumbnail");
+        Property p4 = model.createProperty(nsPrefix, "id");
+        model.setNsPrefix( "video", nsPrefix );
+
+        try{
+            URL url = new URL(yt_url);
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            while ((line = br.readLine()) != null) {
+                stringArrayBuilder = stringArrayBuilder.append(line);
+            }
+            stringArray = stringArrayBuilder.toString();
+
+            JSONObject nodeRoot = new JSONObject(stringArray);
+            JSONArray jsonArray = (JSONArray)nodeRoot.get("items");
+
+
+            for(int i=0; i<jsonArray.length();i++){
+                JSONObject obj = jsonArray.getJSONObject(i);
+
+                JSONObject snippet = (JSONObject) obj.get("snippet");
+
+                description = (String) snippet.get("description");
+                titleOfVideo = (String) snippet.get("title");
+
+
+                JSONObject thumbnails = (JSONObject)snippet.get("thumbnails");
+                JSONObject thumbnail = (JSONObject)thumbnails.get("high");
+                thumbnailURL = (String) thumbnail.get("url");
+
+                JSONObject id = (JSONObject) obj.get("id");
+                videoId = (String) id.get("videoId");
+
+
+                Resource video = model.createResource(nsPrefix + "video"+i);
+                video.addProperty(p1, titleOfVideo, XSDDatatype.XSDstring);
+                video.addProperty(p2, description, XSDDatatype.XSDstring);
+                video.addProperty(p3, thumbnailURL, XSDDatatype.XSDanyURI);
+                video.addProperty(p4, videoId, XSDDatatype.XSDstring);
+
+            }
+
+            FileWriter out = new FileWriter(keyword+".xml" );
+            model.write( out, "RDF/XML" );
 
 
         }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
+
+    public void readVideosFromRDF(String keyword){
+        Model model = ModelFactory.createDefaultModel();
+        model.read(keyword+".xml", "RDF/XML");
+
+        RDFDataMgr.write(System.out, model, Lang.RDFXML);
 
     }
 }
